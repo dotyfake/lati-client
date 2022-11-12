25be149033baff5730144239a2a16b753e42b23f
Doty <douthanh97@gmail.com>
2022-11-13 05:07:12 +0700

    fix url posts

--- src/redux/user/userDetailSlice.ts
+++ src/redux/user/userDetailSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const userApi = createApi({
   reducerPath: 'userApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://lati-server.onrender.com/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://lati-server.onrender.com/api' }),
   endpoints: (builder) => ({
     getUser: builder.query<any, string>({
         query: (userId) => ({
