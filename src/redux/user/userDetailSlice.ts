28881bc7420dc97de6b63ab0b6992df97c2f45bf
Doty <douthanh97@gmail.com>
2022-11-13 05:03:45 +0700

    fix url posts

--- src/redux/user/userDetailSlice.ts
+++ src/redux/user/userDetailSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const userApi = createApi({
   reducerPath: 'userApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/' }),
   endpoints: (builder) => ({
     getUser: builder.query<any, string>({
         query: (userId) => ({
