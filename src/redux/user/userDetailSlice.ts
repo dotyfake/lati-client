9b50b65c30a056488bae480950b9682635ad3a54
Doty <douthanh97@gmail.com>
2022-12-12 23:11:54 +0700

    2

--- src/redux/user/userDetailSlice.ts
+++ src/redux/user/userDetailSlice.ts
@@ -2,11 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const userApi = createApi({
   reducerPath: 'userApi',
-<<<<<<< HEAD
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://lati-server.onrender.com/api' }),
-=======
+
   baseQuery: fetchBaseQuery({ baseUrl: `${process.env.REACT_APP_PUBLIC_URL_HTTPS}/api` }),
->>>>>>> dev
   endpoints: (builder) => ({
     getUser: builder.query<any, string>({
         query: (userId) => ({
