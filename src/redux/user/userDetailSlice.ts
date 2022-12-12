de37c59f9ba2eb5c5dae95684aa96432a7b900a5
Doty <douthanh97@gmail.com>
2022-12-12 23:37:17 +0700

    v1.02

--- src/redux/user/userDetailSlice.ts
+++ src/redux/user/userDetailSlice.ts
@@ -2,7 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const userApi = createApi({
   reducerPath: 'userApi',
-
   baseQuery: fetchBaseQuery({ baseUrl: `${process.env.REACT_APP_PUBLIC_URL_HTTPS}/api` }),
   endpoints: (builder) => ({
     getUser: builder.query<any, string>({
